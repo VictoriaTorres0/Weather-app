1a0c40ac36ee02183d322d4b21bd50de6bc788e2
VictoriaTorres0 <TorresVictoriaF@outlook.com>
2024-04-18 12:33:54 -0300

    Se utilizó useState para gestionar el estado del campo de entrada. Adémas se integró un botón de busqueda para activar la función 'pedirdatos' con la ubicación ingresada.

--- src/pages/DropDown.jsx
+++ src/pages/DropDown.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
 import { SlArrowRight } from "react-icons/sl";
-function DropDown() {
+function DropDown({ pedirdatos }) {
+  const [dataUbication, setDataUbication] = useState("");
+  const handleChange = (e) => {
+    setDataUbication(e.target.value);
+  };
   return (
-    <div className="DropDown-container">
+    <div className="DropDown-container bg-[primary] h-screen">
       <div className="flex justify-end">
         <button className="text-white text-[22px] pr-4">x</button>
       </div>
@@ -9,16 +14,23 @@ function DropDown() {
         <div className="">
           <input
             type="text"
+            value={dataUbication}
             placeholder="🔍︎ search location"
+            onChange={handleChange}
             className="bg-primary w-[130%] outline-none text-center border border-[#E7E7EB] p-[7px]"
           />
         </div>
 
-        <button className="bg-[#3C47E9] text-[#E7E7EB] p-[8px]">Search</button>
+        <button
+          className="bg-[#3C47E9] text-[#E7E7EB] p-[8px]"
+          onClick={() => pedirdatos(dataUbication)}
+        >
+          Search
+        </button>
       </div>
       <div className="p-4">
         <button
-          className="bg-primary outline-none border w-[100%] h-[55px] border-[#E7E7EB]"
+          className="bg-primary outline-none border w-[100%] h-[50px] border-[#E7E7EB]"
           type="text"
         >
           <SlArrowRight color="#616475" className="w-[12px]" />
