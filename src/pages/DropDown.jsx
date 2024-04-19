b5a552753d0f2359bbe89ca0e4c447417bd6b279
VictoriaTorres0 <TorresVictoriaF@outlook.com>
2024-04-19 15:34:18 -0300

    Se creó en el componente DropDown un boton para cerrar el modal.

--- src/pages/DropDown.jsx
+++ src/pages/DropDown.jsx
@@ -1,44 +1,56 @@
 import { useState } from "react";
 import { SlArrowRight } from "react-icons/sl";
-function DropDown({ pedirdatos }) {
+import { RxCross2 } from "react-icons/rx";
+import PropTypes from "prop-types";
+export default function DropDown({ onClose }) {
   const [dataUbication, setDataUbication] = useState("");
   const handleChange = (e) => {
     setDataUbication(e.target.value);
   };
-  return (
-    <div className="DropDown-container bg-[primary] h-screen">
-      <div className="flex justify-end">
-        <button className="text-white text-[22px] pr-4">x</button>
-      </div>
-      <div className="pt-5 flex justify-between p-4">
-        <div className="">
-          <input
-            type="text"
-            value={dataUbication}
-            placeholder="🔍︎ search location"
-            onChange={handleChange}
-            className="bg-primary w-[130%] outline-none text-center border border-[#E7E7EB] p-[7px]"
-          />
-        </div>
 
-        <button
-          className="bg-[#3C47E9] text-[#E7E7EB] p-[8px]"
-          onClick={() => pedirdatos(dataUbication)}
-        >
-          Search
-        </button>
-      </div>
-      <div className="p-4">
-        <button
-          className="bg-primary outline-none border w-[100%] h-[50px] border-[#E7E7EB]"
-          type="text"
-        >
-          <SlArrowRight color="#616475" className="w-[12px]" />
-          <p>London</p>
-        </button>
+  return (
+    <div className="modal">
+      <div className="DropDown-container bg-[primary] h-screen">
+        <div className="flex justify-end pt-4">
+          <button
+            onClick={onClose}
+            className="buton-close text-[#E7E7EB] text-[17px] pr-4"
+          >
+            <RxCross2 />
+          </button>
+        </div>
+        <div className="pt-5 flex justify-between p-4">
+          <div className="">
+            <input
+              type="text"
+              value={dataUbication}
+              placeholder="search location"
+              onChange={handleChange}
+              className="bg-primary w-[130%] outline-none text-center border border-[#E7E7EB] p-[7px]"
+            />
+          </div>
+
+          <button
+            className="bg-[#3C47E9] text-[#E7E7EB] p-[8px]"
+            onClick={() => console.log("Buscar")} 
+          >
+            Search
+          </button>
+        </div>
+        <div className="p-4 flex">
+          <button
+            className="bg-primary p-3 justify-between text-center flex outline-none border w-[100%] h-[50px] border-[#616475]"
+            type="text"
+          >
+            <p className="text-[#E7E7EB]">London</p>
+            <SlArrowRight color="#616475" className="w-[12px] h-[100%]" />
+          </button>
+        </div>
       </div>
     </div>
   );
 }
 
-export default DropDown;
+DropDown.propTypes = {
+  onClose: PropTypes.func.isRequired,
+};
