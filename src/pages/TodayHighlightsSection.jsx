7e69761ff033c98da52822f7d257a18a52b74cf0
VictoriaTorres0 <TorresVictoriaF@outlook.com>
2024-05-06 18:31:59 -0300

    Actualicé los estilos para mejorar su apariencia y legibilidad.

--- src/pages/TodayHighlightsSection.jsx
+++ src/pages/TodayHighlightsSection.jsx
@@ -1,10 +1,12 @@
 function TodayHighlightsSection({ title, value, unit, children = undefined }) {
   return (
-    <div className="bg-primary  w-[340px] h-[200px]">
-      <h2 className="text-white">{title}</h2>
+    <div className="bg-primary w-[340px] h-[200px] lg:w-[368px] lg:h-[226px]  flex flex-col justify-center">
+      <h2 className="text-[#E7E7EB] text-[20px] font-normal">{title}</h2>
       <div>
-        <span>{value}</span>
-        <span>{unit}</span>
+        <span className="text-[70px] font-semibold text-[#E7E7EB] pr-3">
+          {value}
+        </span>
+        <span className="text-[#E7E7EB] text-[30px]">{unit}</span>
       </div>
       {children}
     </div>
