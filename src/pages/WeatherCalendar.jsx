d1de01bfa3986456c3b951ddeedecd6e5881df15
VictoriaTorres0 <TorresVictoriaF@outlook.com>
2024-05-06 18:36:06 -0300

    Importé los iconos meteorológicos desde el archivo "Weather-icons.js".  Además mejoré los estilos.

--- src/pages/WeatherCalendar.jsx
+++ src/pages/WeatherCalendar.jsx
@@ -1,55 +1,25 @@
-function WeatherCalendar({ data }) {
+import { motion } from "framer-motion";
+import { WeatherIcons } from "../consts/weather-icons";
+
+function WeatherCalendar({ title, C1, C2, icon, unidad }) {
   return (
-    <div className="justify-center grid grid-rows-3 grid-flow-col gap-4 pt-20 pb-20 md:grid md:grid-rows-1 md:gap-3 md:pb-12">
-      <div className="text-white">
-        <div className="bg-[#E7E7EB] font-semibold text-[#110E3C] rounded-full p-[4px] w-[32px]">
-          <p>°C</p>
-        </div>
-        <div className="bg-[#585676] font-semibold rounded-full p-[4px] w-[32px]">
-          <p>°F</p>
-        </div>
+    <motion.div
+      whileHover={{ scale: 1.1 }}
+      className="bg-[#1E213A] py-3 my-3  grid justify-items-center items-center w-[140px] lg:text-[16px] lg:w-[122px] lg:h-[177px]"
+    >
+      <h2 className="text-[#E7E7EB] lg:text-[20px] lg:font-semibold">
+        {title}
+      </h2>
+      <img className="py-2 lg:w-[65px]" src={WeatherIcons[icon]} alt={title} />
+      <div className="flex text-center justify-center gap-2 ">
+        <p className="text-[#E7E7EB] lg:text-[16px] lg:font-semibold">
+          {Math.floor(C1)}°{unidad === "metric" ? "c" : "f"}
+        </p>
+        <p className="text-[#A09FB1] lg:text-[16px] lg:font-semibold">
+          {Math.floor(C2)}°{unidad === "metric" ? "c" : "f"}
+        </p>
       </div>
-      <div className="bg-[#1E213A] grid items-center w-[120px] h-[177px] md:w-[85px] md:h-[150px] lg:w-[120px] lg:h-[200px]">
-        <p className="text-[#E7E7EB] md:text-[14px]">Tomorrow</p>
-        <img src="/src/images/snow.png" alt="" />
-        <div className="flex text-center justify-center gap-2">
-          <p className="text-[#E7E7EB] md:text-[13px]">16°c</p>
-          <p className="text-[#A09FB1] md:text-[13px]">11°C</p>
-        </div>
-      </div>
-      <div className="bg-[#1E213A] grid items-center w-[120px] h-[177px] md:w-[85px] md:h-[150px] lg:w-[120px] lg:h-[200px]">
-        <p className="text-[#E7E7EB] md:text-[14px]">Mon, 8 Jun</p>
-        <img src="/src/images/snow.png" alt="" />
-        <div className="flex text-center justify-center gap-2">
-          <p className="text-[#E7E7EB] md:text-[13px]">16°c</p>
-          <p className="text-[#A09FB1] md:text-[13px]">11°C</p>
-        </div>
-      </div>
-      <div className="bg-[#1E213A] grid items-center w-[120px] h-[177px] md:w-[85px] md:h-[150px] lg:w-[120px] lg:h-[200px]">
-        <p className="text-[#E7E7EB] md:text-[14px]">Wed, 10 Jun</p>
-        <img src="/src/images/snow.png" alt="" />
-        <div className="flex text-center justify-center gap-2">
-          <p className="text-[#E7E7EB] md:text-[13px]">16°c</p>{" "}
-          <p className="text-[#A09FB1] md:text-[13px]">11°C</p>
-        </div>
-      </div>
-      <div className="bg-[#1E213A] grid items-center w-[120px] h-[177px] md:w-[85px] md:h-[150px] lg:w-[120px] lg:h-[200px]">
-        <p className="text-[#E7E7EB] md:text-[14px]">Sun, 7 Jun</p>
-        <img src="/src/images/snow.png" alt="" />
-        <div className="flex text-center justify-center gap-2">
-          <p className="text-[#E7E7EB] md:text-[13px]">16°c</p>{" "}
-          <p className="text-[#A09FB1] md:text-[13px]">11°C</p>
-        </div>
-      </div>
-      <div className="bg-[#1E213A] grid items-center w-[120px] h-[177px] md:w-[85px] md:h-[150px] lg:w-[120px] lg:h-[200px]">
-        <p className="text-[#E7E7EB] md:text-[14px]">Tue, 9 Jun</p>
-        <img src="/src/images/snow.png" alt="" />
-        <div className="flex text-center justify-center gap-2">
-          <p className="text-[#E7E7EB] md:text-[13px]">16°c</p>{" "}
-          <p className="text-[#A09FB1] md:text-[13px]">11°C</p>
-        </div>
-      </div>
-    </div>
+    </motion.div>
   );
 }
 
