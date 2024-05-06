a0054e6798e82e570f3d95cb52ffffe8f7c38e0b
VictoriaTorres0 <TorresVictoriaF@outlook.com>
2024-05-06 17:48:34 -0300

    Se agregaron nuevos iconos de clima, estos iconos proporcionan una representación de las condiciones climáticas en la interfaz de usuario.

---
+++ src/consts/weather-icons.js
@@ -0,0 +1,19 @@
+// @ts-nocheck
+import Clear from "../images/Clear.png";
+import HeavyCloud from "../images/HeavyCloud.png";
+import HeavyRain from "../images/HeavyRain.png";
+import LightCloud from "../images/LightCloud.png";
+import Shower from "../images/Shower.png";
+import Thunderstorm from "../images/Thunderstorm.png";
+import Snow from "../images/Snow.png";
+
+export const WeatherIcons = {
+  "01": Clear,
+  "02": LightCloud,
+  "03": HeavyCloud,
+  "04": HeavyCloud,
+  "09": HeavyRain,
+  10: Shower,
+  11: Thunderstorm,
+  13: Snow,
+};
